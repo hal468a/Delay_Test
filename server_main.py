a076bfd3aae844902ed4de8b7f73b7096a0cd59f
hal468a <asdfg880226@gmail.com>
2024-03-09 21:01:44 +0800

    update port

--- server_main.py
+++ server_main.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
 
     # 添加參數
     parser.add_argument('--ip', type=str, help='server ip', default="127.0.0.1")
-    parser.add_argument('--port', type=int, help='server port', default=14550)
+    parser.add_argument('--port', type=int, help='server port', default=14551)
 
     args = parser.parse_args()
 
